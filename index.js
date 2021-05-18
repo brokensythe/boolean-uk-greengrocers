03edb586db2237e420756d2b301f1e8bda6ca635
brokensythe <ade_1.0@live.co.uk>
2021-05-18 23:37:04 +0100

    site has functionality

--- index.js
+++ index.js
@@ -37,112 +37,132 @@ This is how an item object should look like
 // - Create a state object
 
 const state = {
-  products : 
-  [
-  {
-    id: 1,
-    name: "beetroot",
-    img: String.raw`assets/icons/001-beetroot.svg`,
-    price : 1.2
-  },
-  {
-    id: 2,
-    name: "carrot",
-    img: String.raw`assets/icons/002-carrot.svg`,
-    price : 1.4
-  },
-  {
-    id: 3,
-    name: "apple",
-    img: String.raw`assets/icons/003-apple.svg`,
-    price : 2.2
-  },
-  {
-    id: 4,
-    name: "apricot",
-    img: String.raw`assets/icons/004-apricot.svg`,
-    price : 0.6
-  },
-  {
-    id: 5,
-    name: "avocado",
-    img: String.raw`assets/icons/005-avocado.svg`,
-    price : 0.8
-  },
-  {
-    id: 6,
-    name: "bananas",
-    img: String.raw`assets/icons/006-bananas.svg`,
-    price : 1.3
-  },
-  {
-    id: 7,
-    name: "bell-pepper",
-    img: String.raw`assets/icons/007-bell-pepper.svg`,
-    price : 1.7
-  },
-  {
-    id: 8,
-    name: "berry",
-    img: String.raw`assets/icons/008-berry.svg`,
-    price : 1.4
-  },
-  {
-    id: 9,
-    name: "blueberry",
-    img: String.raw`assets/icons/009-blueberry.svg`,
-    price : 1.2
-  },
-  {
-    id: 10,
-    name: "eggplant",
-    img: "assets/icons/010-eggplant.svg",
-    price : 1.8
-  }
-  ],
+  products:
+    [
+      {
+        id: 1,
+        name: "beetroot",
+        img: String.raw`assets/icons/001-beetroot.svg`,
+        price: 1.2
+      },
+      {
+        id: 2,
+        name: "carrot",
+        img: String.raw`assets/icons/002-carrot.svg`,
+        price: 1.4
+      },
+      {
+        id: 3,
+        name: "apple",
+        img: String.raw`assets/icons/003-apple.svg`,
+        price: 2.2
+      },
+      {
+        id: 4,
+        name: "apricot",
+        img: String.raw`assets/icons/004-apricot.svg`,
+        price: 0.6
+      },
+      {
+        id: 5,
+        name: "avocado",
+        img: String.raw`assets/icons/005-avocado.svg`,
+        price: 0.8
+      },
+      {
+        id: 6,
+        name: "bananas",
+        img: String.raw`assets/icons/006-bananas.svg`,
+        price: 1.3
+      },
+      {
+        id: 7,
+        name: "bell-pepper",
+        img: String.raw`assets/icons/007-bell-pepper.svg`,
+        price: 1.7
+      },
+      {
+        id: 8,
+        name: "berry",
+        img: String.raw`assets/icons/008-berry.svg`,
+        price: 1.4
+      },
+      {
+        id: 9,
+        name: "blueberry",
+        img: String.raw`assets/icons/009-blueberry.svg`,
+        price: 1.2
+      },
+      {
+        id: 10,
+        name: "eggplant",
+        img: "assets/icons/010-eggplant.svg",
+        price: 1.8
+      }
+    ],
 
-inCart: []
+  inCart: []
 }
 
 
 const itemList = document.querySelector(".item-list.store--item-list")
-
-function renderProducts () {
-for (const fruit of state.products){ 
-  const listEl = createListEl(fruit)
-  itemList.append(listEl)
-}
-}
-
 const cartList = document.querySelector(".item-list.cart--item-list")
+const totalNumber = document.querySelector(".total-number")
 
 
-
-function addProductToCart (productItem) {
-
-  const inCartInfo = {
-    productId: productItem.id,
-    quantity: 1,
-    productPrice: productItem.price
+function renderProducts() {
+  for (const fruit of state.products) {
+    const listEl = createListEl(fruit)
+    itemList.append(listEl)
   }
+}
 
-  let productIsInCart = false
 
-  for (const product of state.inCart) {
-    if (product.productId === productItem.id) {
-      productIsInCart = true
-      ++product.quantity
-      product.productPrice = product.quantity * productItem.price
-    } 
-  }
-  if (!productIsInCart) {
+
+
+// input: productItem
+// action: add any cart items to cart/add one to the quantity in cart
+// output: undefined
+function addProductToCart(productItem) {
+  // check if productItem is in the cart
+
+  const foundItem = state.inCart.find(function (cartItem) {
+    return productItem.id === cartItem.productId
+  })
+  if (foundItem) {
+    ++foundItem.quantity
+    foundItem.productPrice = foundItem.quantity * productItem.price
+  } else {
+    const inCartInfo = {
+      productId: productItem.id,
+      quantity: 1,
+      productPrice: productItem.price,
+      name: productItem.name,
+      img: productItem.img
+    }
     state.inCart.push(inCartInfo)
   }
+
+  // if the item is in the cart increase quantity by one
+  // otherwise add the item to the cart
+
+  // let productIsInCart = false
+
+  // for (const product of state.inCart) {
+  //   if (product.productId === productItem.id) {
+  //     productIsInCart = true
+  //     ++product.quantity
+  //     product.productPrice = product.quantity * productItem.price
+  //   }
+  // }
+  // if (!productIsInCart) {
+  //   state.inCart.push(inCartInfo)
+  // }
+
 }
-  
 
 
-function createListEl (item) {
+function createListEl(item) {
   const listEl = document.createElement("li")
   const imgEl = document.createElement("img")
   imgEl.setAttribute("src", item.img)
@@ -150,37 +170,96 @@ function createListEl (item) {
   const cartBtn = document.createElement("button")
   cartBtn.innerText = `Add to Basket`
 
-  
-  
+
+
   listEl.append(imgEl, cartBtn)
-  
-  cartBtn.addEventListener("click" , function(){
+
+  cartBtn.addEventListener("click", function () {
     addProductToCart(item)
-    console.log(state.inCart)
+    createCartItems()
+    renderTotal()
+    console.log("cart list:", state.inCart)
   })
   return listEl
 }
 
 
+function createCartItem(item) {
+  const listCartEl = document.createElement("li")
+  const imgCartEl = document.createElement("img")
+  imgCartEl.setAttribute("class", "cart--item-icon")
+  imgCartEl.setAttribute("src", item.img)
+  imgCartEl.setAttribute("alt", item.name)
+  const pCartEl = document.createElement("p")
+  pCartEl.innerText = item.name
 
-// <li>
-//   <img
-//     class="cart--item-icon"
-//     src="assets/icons/001-beetroot.svg"
-//     alt="beetroot"
-//   />
-//   <p>beetroot</p>
-//   <button class="quantity-btn remove-btn center">-</button>
-//   <span class="quantity-text center">1</span>
-//   <button class="quantity-btn add-btn center">+</button>
-// </li>
-
-
-
-
-
-
+  const minusBtn = document.createElement("button")
+  minusBtn.setAttribute("class", "quantity-btn remove-btn center")
+  minusBtn.innerText = "-"
+  minusBtn.addEventListener("click", function () {
+    decreaseQuantity(item)
+    console.log("In Cart:", state.inCart)
+  })
 
 
+  const spanCartEl = document.createElement("span")
+  spanCartEl.setAttribute("class", "quantity-text center")
+  spanCartEl.innerText = item.quantity
+  const plusBtn = document.createElement("button")
+  plusBtn.setAttribute("class", "quantity-btn add-btn center")
+  plusBtn.innerText = "+"
+  plusBtn.addEventListener("click", function () {
+    const singlePrice = item.productPrice / item.quantity
+    item.quantity++
+    spanCartEl.innerText = item.quantity
+    item.productPrice = singlePrice * item.quantity
+    renderTotal()
+  })
 
-renderProducts ()
+  listCartEl.append(imgCartEl, pCartEl, minusBtn, spanCartEl, plusBtn)
+
+  return listCartEl
+}
+
+function createCartItems() {
+  cartList.innerHTML = ""
+  for (const item of state.inCart) {
+    const listCartEl = createCartItem(item)
+    cartList.append(listCartEl)
+  }
+}
+
+function renderTotal() {
+  let totalPrice = 0
+  for (const cartItem of state.inCart) {
+    totalPrice += cartItem.productPrice
+  }
+  totalNumber.innerText = `£${totalPrice.toFixed(2)}`
+  console.log("total price:", totalPrice)
+}
+
+function decreaseQuantity(item) {
+  const singlePrice = item.productPrice / item.quantity
+  item.quantity--
+  if (item.quantity === 0) {
+    const indexToDelete = state.inCart.findIndex(function (cartItem) {
+      return item.productId === cartItem.productId
+    })
+    state.inCart.splice(indexToDelete, 1)
+  } else {
+    item.productPrice = singlePrice * item.quantity
+  }
+  createCartItems()
+  renderTotal()
+}
+
+function increaseQuantity(item) {
+  const singlePrice = item.productPrice / item.quantity
+  item.quantity++
+  spanCartEl.innerText = item.quantity
+  item.productPrice = singlePrice * item.quantity
+  renderTotal()
+}
+
+renderProducts()
+renderTotal()
